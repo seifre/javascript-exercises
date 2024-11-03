40ab4579bc37b250fa492a6f551cdacc394afa07
seifre <seiframram75@gmail.com>
2024-11-03 18:35:24 +0100

    done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(dgree) {
+  let cdgree = (dgree-32)*(5/9);
+  let rounded = parseFloat(cdgree.toFixed(1));
+  return rounded;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(dgree) {
+  let fdgree = dgree * (9/5)+32 ;
+  let rounded = parseFloat(fdgree.toFixed(1));
+  return rounded;
 };
 
 // Do not edit below this line
